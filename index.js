5f1e2cdac9f56a25e86717030b5b852acd7f66d1
sjo <a@a>
2023-05-09 10:43:14 +0200

    sgty

--- index.js
+++ index.js
@@ -1,35 +1,136 @@
-const ruleta = document.getElementById("ruleta")
 
-const uno= {
-    nombre: "Uno",
-    probabilidad: 20
+
+
+var options = ["hola",4]
+
+
+
+
+var startAngle = 0;
+var arc = Math.PI / (options.length / 2);
+var spinTimeout = null;
+
+var spinArcStart = 10;
+var spinTime = 0;
+var spinTimeTotal = 0;
+
+var ctx;
+
+document.getElementById("spin").addEventListener("click", spin);
+
+function byte2Hex(n) {
+    var nybHexString = "0123456789ABCDEF";
+    return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
 }
 
-const dos= {
-    nombre: "Dos",
-    probabilidad: 50
+function RGB2Color(r,g,b) {
+    return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
 }
 
-const tres= {
-    nombre: "Tres",
-    probabilidad: 30
+function getColor(item, maxitem) {
+    var phase = 0;
+    var center = 128;
+    var width = 127;
+    var frequency = Math.PI*2/maxitem;
+
+    red   = Math.sin(frequency*item+2+phase) * width + center;
+    green = Math.sin(frequency*item+0+phase) * width + center;
+    blue  = Math.sin(frequency*item+4+phase) * width + center;
+
+    return RGB2Color(red,green,blue);
 }
 
-let conceptos = [uno,dos,tres];
+function drawRouletteWheel() {
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        var outsideRadius = 200;
+        var textRadius = 160;
+        //circulo central
+        var insideRadius = 100;
 
-const colores = ["126253","134526","C7B446","5D9B9B","8673A1","100000","4C9141","8E402A","231A24","424632","1F3438","025669","008F39","763C28"];
-function ajustarRuleta(){
-    const opcionesContainer = document.createElement("div");
-    opcionesContainer.id = "opcionesContainer"
-    ruleta.appendChild(opcionesContainer);
-    conceptos.forEach((concepto, i )=>{
-        //crar triangulos colores
-        const opcionesElement = document.createElement("div");
-        opcionesElement.classList.add("opcion");
-        ruleta.appendChild(opcionesElement);
-        opcionesElement.style = `background-color: ${colores[i]}`
-    })
+        ctx = canvas.getContext("2d");
+        ctx.clearRect(0,0,500,500);
 
+        ctx.strokeStyle = "grey";
+        ctx.lineWidth = 1;
+
+        ctx.font = 'bold 12px Helvetica, Arial';
+
+        for(var i = 0; i < options.length; i++) {
+            var angle = startAngle + i * arc;
+            //ctx.fillStyle = colors[i];
+            ctx.fillStyle = getColor(i, options.length);
+
+            ctx.beginPath();
+            ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+            ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+            ctx.stroke();
+            ctx.fill();
+
+            ctx.save();
+            ctx.shadowOffsetX = -1;
+            ctx.shadowOffsetY = -1;
+            ctx.shadowBlur    = 15;
+            ctx.shadowColor   = "rgba(0,0,0,0.9)";
+            ctx.fillStyle = "white";
+            ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
+                250 + Math.sin(angle + arc / 2) * textRadius);
+            ctx.rotate(angle + arc / 2 + Math.PI / 2);
+            var text = options[i];
+            ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+            ctx.restore();
+        }
+
+        //Arrow
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+        ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+        ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
+        ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
+        ctx.fill();
+    }
 }
 
-ajustarRuleta();
+function spin() {
+    spinAngleStart = Math.random() * 10 + 10;
+    spinTime = 0;
+    spinTimeTotal = Math.random() * 3 + 4 * 1000;
+    rotateWheel();
+}
+
+function rotateWheel() {
+    spinTime += 30;
+    if(spinTime >= spinTimeTotal) {
+        stopRotateWheel();
+        return;
+    }
+    var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+    startAngle += (spinAngle * Math.PI / 180);
+    drawRouletteWheel();
+    spinTimeout = setTimeout('rotateWheel()', 30);
+}
+
+function stopRotateWheel() {
+    clearTimeout(spinTimeout);
+    var degrees = startAngle * 180 / Math.PI + 90;
+    var arcd = arc * 180 / Math.PI;
+    var index = Math.floor((360 - degrees % 360) / arcd);
+    ctx.save();
+    ctx.font = 'bold 30px Helvetica, Arial';
+    var text = options[index]
+    ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
+    ctx.restore();
+}
+
+function easeOut(t, b, c, d) {
+    var ts = (t/=d)*t;
+    var tc = ts*t;
+    return b+c*(tc + -3*ts + 3*t);
+}
+
+drawRouletteWheel();
